45a3dcd83f6fb9066c80e40af4f7aec3b8c2b1f0
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2019-05-21 20:05:34 -0300

    Adicionando suporte total ao BOCA

--- arquivos/src/generator.cpp
+++ arquivos/src/generator.cpp
@@ -1,28 +1,94 @@
 #include "testlib.h"
 #include <bits/stdc++.h>
 
-const int number_of_manual_tests = 3;
-const int number_of_tests = 60;
+using namespace std;
 
-void manual_tests(){
-    startTest(1);
-    // Print manual test 1
-    startTest(2);
-    // Print manual test 2
-    startTest(3);
-    // Print manual test 3
+using vi = vector<int>;
+
+const int MIN_N = 0;
+const int MAX_N = 100;
+
+const int MIN_M = 1;
+const int MAX_M = 1e9;
+
+const int MIN_V = 0;
+const int MAX_V = 1e9;
+
+const int rnd_test_n = 100;
+
+template <typename T> void append(vector<T> &dest, const vector<T> &orig) {
+	dest.insert(dest.end(), orig.begin(), orig.end());
 }
 
-void generate_test(int i){
-    startTest(i);
+string output_tc(int x,int y) {
+	ostringstream oss;
+    oss << x << " " << y << endl;
+	return oss.str();
 }
 
 
-int main(int argc, char* argv[]){
-    //The first 3 tests are manually generated
-    manual_tests();
-    registerGen(argc, argv, 1);
-    for(uint64_t i=number_of_manual_tests+1;i<=number_of_tests;i++){
-        generate_test(i);
-    }    
+vector<string> generate_sample_tests() {
+	vector<string> tests;
+    tests.push_back(output_tc(1,1));
+    tests.push_back(output_tc(2,2));
+    tests.push_back(output_tc(0,0));
+	return tests;
+}
+
+
+
+vector<string> generate_manual_tests() {
+	vector<string> tests;
+	tests.push_back(output_tc(100,0));
+	tests.push_back(output_tc(0,100));
+	return tests;
+}
+
+string rnd_test(int i){
+    int min_n = MIN_N;
+    int max_n = MAX_N;
+    
+    if(i<rnd_test_n/3){
+        max_n = 5;
+    }
+    else if(i<rnd_test_n/2){
+        max_n = 20;
+    }
+
+    int x = rnd.next(min_n,max_n);
+    int y = rnd.next(min_n,max_n);
+    return(output_tc(x,y));
+}
+
+vector<string> generate_random_tests() { 
+    vector<string> tests;
+    for (int i = 0; i < rnd_test_n; i++){
+        tests.push_back(rnd_test(i));
+    } 
+    return tests;
+}
+
+string extreme_test_1(){
+    return(output_tc(100,100));
+}
+
+vector<string> generate_extreme_tests(){
+    vector<string> tests;   
+    tests.push_back(extreme_test_1());
+    return tests;
+}
+
+int main(int argc, char *argv[]) {
+	registerGen(argc, argv, 1);
+	vector<string> tests;
+	size_t test = 0;
+	append(tests, generate_sample_tests());
+	append(tests, generate_manual_tests());
+	append(tests, generate_random_tests());
+	append(tests, generate_extreme_tests());
+	for (const auto &t : tests) {
+		startTest(++test);
+		cout << t;
+	}
+	return 0;
 }
