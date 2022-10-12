7e440b55c0c0b7dd7e56f19d98fe450d52ac7a48
Matheus Loiola <matheusloiolapinto@gmail.com>
2022-10-12 16:29:32 -0300

    Implemented contest.py tool

--- contest.py
+++ contest.py
@@ -1,31 +1,27 @@
-from operator import mod
-from pdb import main
-from pydoc import doc
 from latexutils import clean_auxiliary_files
 from pdfutils import build_pdf, merge_pdfs
 from boca import boca_pack
-from toolchain import build_checker
+from utils import convert_idx_to_string
+import sys
 import os
+import argparse
+import shutil
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-b', '--boca', action='store_true',
+                        default=False, help='build problems in BOCA format.')
+    parser.add_argument('mode', choices=['build', 'genpdf'], 
+        help='build: create a contest.\n' +
+        'genpdf: generates problem and tutorial PDFs.\n')
+    parser.add_argument('problems_folder', help='problems directory.')
+    parser.add_argument('contest_folder', help='directory which the contest will be saved.')
+    return parser
 
 
 """
-Converts an integer to a string from alphabet [A-Z] using radix 26
-"""
-
-
-def convert_idx_to_string(idx):
-    ans = ''
-    while True:
-        rem = mod(idx, 26)
-        ans += chr(ord('A')+rem)
-        idx //= 26
-        if idx == 0:
-            break
-    return ans
-
-
-"""
-Builds a contest pdf from the PDFs from problem_folder_l
+Builds a contest pdf from the PDFs from the list of problems
 """
 
 
@@ -33,9 +29,10 @@ def build_contest_pdf(problem_folder_l, output_folder):
     print('-Creating contest PDF')
     problem_pdf_l = []
     tutorial_pdf_l = []
-    if not os.path.isdir(output_folder):
-        print(output_folder, ' is not a valid directory')
-        exit(1)
+
+    cls_file = os.path.join(os.path.dirname(__file__), 'arquivos')
+    cls_file = os.path.join(cls_file, 'maratona.cls')
+    shutil.copy(cls_file, output_folder)
 
     for i, folder in enumerate(problem_folder_l):
         label = convert_idx_to_string(i)
@@ -55,6 +52,7 @@ def build_contest_pdf(problem_folder_l, output_folder):
         os.remove(f)
     for f in tutorial_pdf_l:
         os.remove(f)
+    os.remove(os.path.join(output_folder, 'maratona.cls'))
 
 
 """
@@ -62,17 +60,39 @@ Builds BOCA packages from the list of problems
 """
 
 
-def build_boca_packages(problem_folder_l):
+def build_boca_packages(problem_folder_l, output_folder):
     print('-Creating BOCA Files')
     for i, folder in enumerate(problem_folder_l):
         label = convert_idx_to_string(i)
         options = {'display_author': False,
                    'problem_label': label}
         build_pdf(folder, folder, options)
-        clean_auxiliary_files(folder)
-        build_checker(folder)
+        # build_checker(folder)
         boca_pack(folder)
+        boca_file_path = os.path.join(folder, 'boca.zip')
+        boca_file = os.path.join(output_folder, os.path.basename(folder) + '-boca.zip')
+        shutil.copy(boca_file_path, boca_file)
 
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.problems_folder):
+        print(args.problems_folder + ' is not a valid directory')
+        sys.exit(1)
+
+    problems = []
+    for f in os.listdir(args.problems_folder):
+        problems.append(os.path.join(args.problems_folder, f))
+    
+    os.makedirs(args.contest_folder, exist_ok=True)
+
+    if (args.mode == 'build' and args.boca):
+        build_boca_packages(problems, args.contest_folder)
+        build_contest_pdf(problems, args.contest_folder)
+    elif (args.mode == 'build'):
+        build_contest_pdf(problems, args.contest_folder)
+    elif (args.mode == 'genpdf'):
+        build_contest_pdf(problems, args.contest_folder)
+
