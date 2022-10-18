c2644d4128524112652414ce47c5172e75cc9377
Leonam Knupp <leonamknupp@hotmail.com>
2022-10-18 09:03:07 -0300

    Refactor generator.cpp file

--- arquivos/src/generator.cpp
+++ arquivos/src/generator.cpp
@@ -8,19 +8,13 @@ using vi = vector<int>;
 const int MIN_N = 0;
 const int MAX_N = 100;
 
-const int MIN_M = 1;
-const int MAX_M = 1e9;
-
-const int MIN_V = 0;
-const int MAX_V = 1e9;
-
 const int rnd_test_n = 100;
 
 template <typename T> void append(vector<T> &dest, const vector<T> &orig) {
 	dest.insert(dest.end(), orig.begin(), orig.end());
 }
 
-string output_tc(int x,int y) {
+string output_tc(int x, int y) {
 	ostringstream oss;
     oss << x << " " << y << endl;
 	return oss.str();
@@ -29,9 +23,9 @@ string output_tc(int x,int y) {
 
 vector<string> generate_sample_tests() {
 	vector<string> tests;
-    tests.push_back(output_tc(1,1));
-    tests.push_back(output_tc(2,2));
-    tests.push_back(output_tc(0,0));
+    tests.push_back(output_tc(1, 1));
+    tests.push_back(output_tc(2, 2));
+    tests.push_back(output_tc(0, 0));
 	return tests;
 }
 
@@ -39,8 +33,8 @@ vector<string> generate_sample_tests() {
 
 vector<string> generate_manual_tests() {
 	vector<string> tests;
-	tests.push_back(output_tc(100,0));
-	tests.push_back(output_tc(0,100));
+	tests.push_back(output_tc(100, 0));
+	tests.push_back(output_tc(0, 100));
 	return tests;
 }
 
@@ -48,16 +42,16 @@ string rnd_test(int i){
     int min_n = MIN_N;
     int max_n = MAX_N;
     
-    if(i<rnd_test_n/3){
+    if(i < rnd_test_n / 3){
         max_n = 5;
     }
-    else if(i<rnd_test_n/2){
+    else if(i < rnd_test_n / 2){
         max_n = 20;
     }
 
-    int x = rnd.next(min_n,max_n);
-    int y = rnd.next(min_n,max_n);
-    return(output_tc(x,y));
+    int x = rnd.next(min_n, max_n);
+    int y = rnd.next(min_n, max_n);
+    return(output_tc(x, y));
 }
 
 vector<string> generate_random_tests() { 
@@ -69,7 +63,7 @@ vector<string> generate_random_tests() {
 }
 
 string extreme_test_1(){
-    return(output_tc(100,100));
+    return(output_tc(100, 100));
 }
 
 vector<string> generate_extreme_tests(){
