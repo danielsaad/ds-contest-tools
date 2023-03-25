2291f8edb515df7c5bbc9c47d7cd4e648e391c44
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-03-25 12:33:46 -0300

    Module metadata.py updated for better maintenance

--- metadata.py
+++ metadata.py
@@ -2,8 +2,15 @@ from typing import Union
 
 
 def singleton(cls):
-    """Decorator to create a Singleton class."""
-    instances = {}
+    """Decorator to create a Singleton class.
+
+    Args:
+        cls: The class to decorate.
+
+    Returns:
+        A function that returns a singleton instance of the given class.
+    """
+    instances: dict = {}
 
     def get_instance(*args, **kwargs):
         if cls not in instances:
@@ -14,11 +21,24 @@ def singleton(cls):
 
 @singleton
 class Paths:
-    """A class to store the most used paths of the tool."""
+    """A class to store the most commonly used paths of the tool.
+
+    Attributes:
+        dirs: A dictionary containing the paths to the problem and output directories.
+
+    Methods:
+        get_problem_dir() -> Union[str, list]: Returns the path to the problem directory.
+        get_output_dir() -> str: Returns the path to the output directory.
+    """
 
     def __init__(self, problem_dir: Union[str, list], output_dir: str):
-        """Construct all the necessary attributes for the instance."""
-        self.dirs = {
+        """Initializes the necessary attributes for the instance.
+
+        Args:
+            problem_dir : The path to the problem(s) directory(ies).
+            output_dir: The path to the output directory.
+        """
+        self.dirs: dict = {
             "problem_dir": problem_dir,
             "output_dir": output_dir
         }
