9746ea9c7492cefa1fb60e98fd772e378cb94da2
Leonam Knupp <leonamknupp@hotmail.com>
2023-04-10 20:51:04 -0300

    Change html filename to report.html

--- htmlutils.py
+++ htmlutils.py
@@ -286,7 +286,7 @@ def write_footer(f_out: io.TextIOWrapper) -> None:
 
 def print_to_html(problem_metadata: str, solutions_info_dict: dict) -> None:
     problem_folder: str = Paths().get_problem_dir()
-    html_filename: str = 'index.html'
+    html_filename: str = 'report.html'
     html_filepath: str = os.path.join(problem_folder, html_filename)
     info_log(f'Creating {html_filename}')
     with open(html_filepath, 'w') as f_out:
