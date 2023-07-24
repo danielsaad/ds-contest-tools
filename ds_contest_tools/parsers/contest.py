df12b7cc67c53bc9170ee438e219b6452e2699d1
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-07-24 18:13:43 -0300

    feat: added no problem verification flag

--- ds_contest_tools/parsers/contest.py
+++ ds_contest_tools/parsers/contest.py
@@ -3,7 +3,7 @@ from ..metadata import Paths
 from .common import *
 
 
-def process_contest(problems_dir: list, output_dir: str, pdf: bool, io: bool, author:bool) -> None:
+def process_contest(problems_dir: list, output_dir: str, pdf: bool, io: bool, author: bool, no_verify: bool) -> None:
     """
     Process the contest files.
 
@@ -12,11 +12,15 @@ def process_contest(problems_dir: list, output_dir: str, pdf: bool, io: bool, au
         output_dir: output directory.
         pdf: Whether is to generate only contest PDFs.
         io: Whether is to generate only contest input/output files.
+        author: Whether is to add author name to PDFs.
+        no_verify: Whether is to verify and rebuild problems.
     """
     setup_and_validate_paths(problems_dir, output_dir, additional_verification=['pdfjam'])
     os.makedirs(output_dir, exist_ok=True)
-    for problem in Paths().get_problem_dir():
-        verify_problem(problem)
+    
+    if not no_verify:
+        for problem in Paths().get_problem_dir():
+            verify_problem(problem)
 
     info_log('Generating contest files')
     if io:
@@ -44,9 +48,10 @@ def add_parser(subparsers) -> None:
     mut_ex_group.add_argument('-i', '--io', action='store_true',
                                 default=False, help='generate contest input/output files')
     contest_parser.add_argument('--author', action='store_true', help='add author name to PDFs')
+    contest_parser.add_argument('-nv', '--no-verify', action='store_true', help='do not verify and rebuild problems', default=False)
     contest_parser.add_argument(
         'problem_dir', help='path to problem(s)', nargs='+')
     contest_parser.add_argument(
         'contest_dir', help='directory which the contest will be saved')
     contest_parser.set_defaults(function=lambda options: process_contest(
-        options.problem_dir, options.contest_dir, options.pdf, options.io, options.author))
+        options.problem_dir, options.contest_dir, options.pdf, options.io, options.author, options.no_verify))
