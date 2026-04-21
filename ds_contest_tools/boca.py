8e2c195c09c50ad358615f3bda038f22c9b59b2a
Daniel Saad <daniel.saad.nunes@gmail.com>
2026-04-21 17:56:39 -0300

    Forbid sending .interactive files to BOCA input during conversion

--- ds_contest_tools/boca.py
+++ ds_contest_tools/boca.py
@@ -17,7 +17,7 @@ class default_boca_limits:
 
 def boca_zip(boca_folder: str) -> None:
     """ Zip a problem of BOCA format.
-    
+
     Args:
         boca_folder: Path to the BOCA folder.
     """
@@ -34,25 +34,32 @@ def boca_zip(boca_folder: str) -> None:
 
 def boca_pack(problem_folder: str, output_folder: str) -> None:
     """Convert a DS problem to a BOCA problem.
-    
+
     Args:
         problem_folder: Path to the problem folder.
         output_folder: Path to the output folder.
     """
     # Verify if it is a conversion from a contest
-    info_log(f"Starting BOCA conversion for problem {os.path.basename(problem_folder)}.")
+    info_log(
+        f"Starting BOCA conversion for problem {os.path.basename(problem_folder)}.")
     os.makedirs(output_folder, exist_ok=True)
 
-    boca_template_folder = os.path.join(
-        *[os.path.dirname(os.path.abspath(__file__)), 'files', 'boca'])
-    boca_folder = os.path.join(*[output_folder, 'boca'])
-    
-    # Copy template files
-    recursive_overwrite(boca_template_folder, boca_folder)
-    
     # Get problem metadata
     problem_metadata = parse_json(os.path.join(problem_folder, 'problem.json'))
     check_problem_metadata(problem_metadata)
+
+    # Get BOCA template and create BOCA folder
+    if problem_metadata['problem']['interactive'] == False:
+        boca_template_folder = os.path.join(
+            *[os.path.dirname(os.path.abspath(__file__)), 'files', 'boca'])
+    else:
+        boca_template_folder = os.path.join(
+            *[os.path.dirname(os.path.abspath(__file__)), 'files', 'boca', 'interactive'])
+    boca_folder = os.path.join(*[output_folder, 'boca'])
+
+    # Copy template files
+    recursive_overwrite(boca_template_folder, boca_folder)
+
     basename = os.path.basename(os.path.abspath(problem_folder))
     filename = os.path.join(problem_folder, basename)
     boca_description_folder = os.path.join(boca_folder, 'description')
@@ -69,13 +76,14 @@ def boca_pack(problem_folder: str, output_folder: str) -> None:
     checker_boca = os.path.join(problem_folder, 'bin', 'checker-boca')
     verify_path(checker_boca)
     compare_folder = os.path.join(boca_folder, 'compare')
-    checker_compare = os.path.join(compare_folder, 'checker-boca')
-    shutil.copy2(checker_boca, checker_compare)
-    shutil.copy2(checker_compare, os.path.join(compare_folder, 'c'))
-    shutil.copy2(checker_compare, os.path.join(compare_folder, 'cpp'))
-    shutil.copy2(checker_compare, os.path.join(compare_folder, 'java'))
-    shutil.copy2(checker_compare, os.path.join(compare_folder, 'py3'))
-    shutil.copy2(checker_compare, os.path.join(compare_folder, 'kt'))
+    if problem_metadata['problem']['interactive'] == False:
+        checker_compare = os.path.join(compare_folder, 'checker-boca')
+        shutil.copy2(checker_boca, checker_compare)
+        shutil.copy2(checker_compare, os.path.join(compare_folder, 'c'))
+        shutil.copy2(checker_compare, os.path.join(compare_folder, 'cpp'))
+        shutil.copy2(checker_compare, os.path.join(compare_folder, 'java'))
+        shutil.copy2(checker_compare, os.path.join(compare_folder, 'py3'))
+        shutil.copy2(checker_compare, os.path.join(compare_folder, 'kt'))
 
     # Limits
     java_python_time_factor = 3
@@ -84,7 +92,7 @@ def boca_pack(problem_folder: str, output_folder: str) -> None:
             time_limit = problem_metadata['boca_config']['time_limit']
             if filename in ['java', 'py2', 'py3']:
                 time_limit *= java_python_time_factor
-            
+
             # Get limits from problem.json or use default values, if not specified
             repetitions = problem_metadata['boca_config']['number_of_repetitions']
             repetitions = repetitions if repetitions is not None else default_boca_limits.number_of_repetitions
@@ -92,7 +100,7 @@ def boca_pack(problem_folder: str, output_folder: str) -> None:
             max_memory = max_memory if max_memory is not None else default_boca_limits.maximum_memory
             max_output = problem_metadata['boca_config']['maximum_output_size_kb']
             max_output = max_output if max_output is not None else default_boca_limits.maximum_output_size
-            
+
             f.write('echo ' + str(time_limit) + '\n')
             f.write('echo ' + str(repetitions)+'\n')
             f.write('echo ' + str(max_memory)+'\n')
@@ -104,7 +112,7 @@ def boca_pack(problem_folder: str, output_folder: str) -> None:
     problem_input_folder = os.path.join(problem_folder, 'input')
     os.makedirs(boca_input_folder, exist_ok=True)
     input_files = [os.path.join(problem_input_folder, f) for
-                   f in os.listdir(problem_input_folder) if os.path.isfile(os.path.join(problem_input_folder, f))]
+                   f in os.listdir(problem_input_folder) if os.path.isfile(os.path.join(problem_input_folder, f)) and not f.endswith('.interactive')]
     for filename in input_files:
         shutil.copy2(filename, boca_input_folder)
     rename_io(boca_input_folder)
